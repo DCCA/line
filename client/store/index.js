417863369e9ee3d76f096ae489c2588002890fe0
dcca <dcca12@gmail.com>
2020-06-24 12:34:30 -0300

    add store to client

---
+++ client/store/index.js
@@ -0,0 +1,9 @@
+export const state = () => {
+  userId: null;
+};
+export const mutations = () => {};
+export const actions = {
+  signUp(context, payload) {
+    const { name, email, password } = payload;
+  }
+};
